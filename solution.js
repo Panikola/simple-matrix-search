5e8e6d0eb67fad62eb0f0c2270c3d2f70899227b
paxa <pavel.kanavalau@gmail.com>
2019-04-20 13:35:16 +0300

    solution 1

--- solution.js
+++ solution.js
@@ -1,4 +1,20 @@
 function findWord(text, data) {
   // ваше решение пишите здесь
-  return false;
+  let newData = [];
+  let textReverse = [...text].reverse().join('');
+  let check = row => row.includes(text) ? true : row.includes(textReverse);
+  let result = data.some(row => check(row) ? true : !newData.push([...row]));
+  if(!result) {
+    result = newData[0].some((rowEl, i) => {
+      let columnString = '';
+      let rowI = 0;
+      while (rowI < newData.length) {
+        columnString += newData[rowI][i];
+        rowI ++
+      }
+      return check(columnString)
+    })
+  }
+
+  return result && text
 }
